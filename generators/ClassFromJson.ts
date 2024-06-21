fb1a47eb2c3f5c84a08df5ca3bd286fa04cd6661
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-22 05:39:01 +0800

    Add Format parameter to parseWrapper function

--- generators/ClassFromJson.ts
+++ generators/ClassFromJson.ts
@@ -1,4 +1,4 @@
-import { DartType, Definition } from "./types";
+import { DartType, Definition, Format } from "./types";
 import { getDartTypeByFormat } from "./utils";
 
 export const generateFromJsonMethod = (
@@ -14,6 +14,7 @@ export const generateFromJsonMethod = (
     const dartType = getDartTypeByFormat(properties[propertyName].format);
     code += `${propertyName}: ${parseWrapper(
       dartType,
+      properties[propertyName].format,
       `json['${propertyName}']`,
       isRequired
     )},\n`;
@@ -25,6 +26,7 @@ export const generateFromJsonMethod = (
 
 export const parseWrapper = (
   dartType: DartType,
+  format: Format,
   jsonValue: string,
   required: boolean
 ): string => {
@@ -37,6 +39,12 @@ export const parseWrapper = (
       case "double":
         return `double.parse(${jsonValue}.toString())`;
       case "DateTime":
+        if (format === "time without time zone") {
+          return `DateTime.parse("1970-01-01T\${${jsonValue}}")`;
+        }
+        if (format === "time with time zone") {
+          return `DateTime.parse("1970-01-01T\${${jsonValue}}")`;
+        }
         return `DateTime.parse(${jsonValue})`;
       default:
         return jsonValue;
@@ -51,6 +59,12 @@ export const parseWrapper = (
     case "double":
       return `${jsonValue} != null ? double.tryParse(${jsonValue}.toString()) : null`;
     case "DateTime":
+      if (format === "time without time zone") {
+        return `${jsonValue} != null ? DateTime.tryParse("1970-01-01T\${${jsonValue}}") : null`;
+      }
+      if (format === "time with time zone") {
+        return `${jsonValue} != null ? DateTime.tryParse("1970-01-01T\${${jsonValue}}") : null`;
+      }
       return `${jsonValue} != null ? DateTime.tryParse(${jsonValue}) : null`;
     default:
       return jsonValue;
