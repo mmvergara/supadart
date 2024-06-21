bfa2fa33b863a22249b3654eb43a264d75606a85
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-21 18:53:09 +0800

    Update imports and comment out test functions

--- test/test/supadart_test.dart
+++ test/test/supadart_test.dart
@@ -1,9 +1,11 @@
 import 'package:dotenv/dotenv.dart';
 import 'package:supabase/supabase.dart';
-import 'package:supadart_test/generated_classes.dart';
 import 'datatypes/big_int.dart';
-import 'package:test/expect.dart';
-import 'package:test/scaffolding.dart';
+
+import 'datatypes/big_serial.dart';
+import 'datatypes/bit.dart';
+import 'datatypes/bit_varying.dart';
+import 'datatypes/box.dart';
 
 void main() async {
   var env = DotEnv(includePlatformEnvironment: true)..load();
@@ -18,38 +20,9 @@ void main() async {
 
   final supabase = SupabaseClient(url, anonKey);
 
-  // Cleanup
-  try {
-    await supabase.all_types.delete().neq("bigintx", "0");
-  } catch (err) {
-    print("Error cleaning up");
-    print(err);
-  }
-  // =================================================================
-  // =================================================================
-  // Note: For all of the the "INSERT" test we are providing the
-  // serial types with 0 as by default they are not nullable.
-  // or maybe i just don't know postgresql well enough.
-  //
-  // The Supabase API also that we are using marked them as required
-  // =================================================================
-  // =================================================================
-
-  test('Testing BigInt Create,Update,Read', () async {
-    // Create
-    BigInt val = BigInt.from(4221312931259329921);
-    var createResult = await createBigInt(supabase);
-    expect(createResult, null);
-
-    // Update
-    BigInt updatedVal = BigInt.from(4221312931259329821);
-    var updateResult = await updateBigInt(supabase, val, updatedVal);
-    expect(updateResult, null);
-
-    // Read
-    var readResult = await readBigInt(supabase);
-    assert(readResult is List<All_types>);
-    expect(readResult!.length, 1);
-    expect(readResult[0].bigintx, updatedVal);
-  });
+  // await performBigIntTest(supabase);
+  // await performBigSerialTests(supabase);
+  // await performBitTest(supabase);
+  // await performBitVaryingTest(supabase);
+  await performBoxTest(supabase);
 }
