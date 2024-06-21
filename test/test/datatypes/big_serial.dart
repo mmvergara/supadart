b85d3f2e1be1629a4c91ce786f809c1b879476b7
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-21 18:53:02 +0800

    Refactor big serial tests and functions

--- test/test/datatypes/big_serial.dart
+++ test/test/datatypes/big_serial.dart
@@ -1,50 +1,67 @@
-// import 'package:supabase/supabase.dart';
-// import 'package:supadart_test/generated_classes.dart'; // Assuming this imports All_types class
+import 'package:supabase/supabase.dart';
+import 'package:supadart_test/generated_classes.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+import '../cleanup.dart';
 
-// // Function to create a new row with bigserial value (auto-increment)
-// Future<Object?> createBigserial(SupabaseClient supabase) async {
-//   try {
-//     // No value needs to be explicitly set for bigserial as it auto-increments
-//     final response = await supabase.all_types.insert(All_types.insert());
-//     return response; // You might want to return the inserted data (ID)
-//   } catch (error) {
-//     return error;
-//   }
-// }
+Future<void> performBigSerialTests(SupabaseClient supabase) async {
+  BigInt insertBigSerial = BigInt.from(4221312931259329921);
+  BigInt updatedBigSerial = BigInt.from(4221312931259329821);
+  test('Testing BigSerial Create', () async {
+    await cleanup(supabase);
+    var createResult = await createBigSerial(supabase, insertBigSerial);
+    expect(createResult, null);
+  });
 
-// // Function to read all rows with bigserial values
-// Future<List<All_types>?> readBigserial(SupabaseClient supabase) async {
-//   try {
-//     final result = await supabase.all_types.select().execute();
-//     if (result.error != null) {
-//       throw result.error!; // Re-throw the Supabase error
-//     }
-//     final data = result.data as List;
-//     return data.map((item) => All_types.fromJson(item)).toList();
-//   } catch (error) {
-//     print("readBigserial error");
-//     print(error);
-//     return null;
-//   }
-// }
+  test('Testing BigSerial Update', () async {
+    var updateResult =
+        await updateBigSerial(supabase, insertBigSerial, updatedBigSerial);
+    expect(updateResult, null);
+  });
 
-// // Function to update a row based on the existing bigserial value (assuming it's the primary key)
-// Future<Object?> updateBigserial(
-//     SupabaseClient supabase, All_types existingData) async {
-//   try {
-//     // Update other fields as needed (excluding bigserialx)
-//     final updateData = existingData
-//         .copyWith(); // Create a copy to avoid modifying original data
-//     updateData.bigintx =
-//         BigInt.from(100); // Replace with your desired update value
+  test('Testing BigSerial Read', () async {
+    var readResult = await readBigSerial(supabase);
+    assert(readResult is List<All_types>);
+    expect(readResult!.length, 1);
+    expect(readResult[0].bigserialx, updatedBigSerial);
+  });
+}
 
-//     final response = await supabase.all_types.update(updateData.update()).eq(
-//         'bigserialx',
-//         existingData.bigserialx); // Use existing bigserialx as the filter
-//     return response; // You might want to return the updated data
-//   } catch (error) {
-//     print("updateBigserial error");
-//     print(error);
-//     return error;
-//   }
-// }
+Future<Object?> createBigSerial(
+    SupabaseClient supabase, BigInt insertVal) async {
+  try {
+    await supabase.all_types.insert(All_types.insert(
+      bigserialx: insertVal,
+    ));
+    return null;
+  } catch (error) {
+    return error;
+  }
+}
+
+Future<List<All_types>?> readBigSerial(SupabaseClient supabase) async {
+  try {
+    var res = await supabase.all_types
+        .select()
+        .withConverter((data) => data.map(All_types.fromJson).toList());
+    return res;
+  } catch (error) {
+    print("readBigSerial error");
+    print(error);
+    return null;
+  }
+}
+
+Future<Object?> updateBigSerial(
+    SupabaseClient supabase, BigInt oldValue, BigInt value) async {
+  try {
+    await supabase.all_types
+        .update(All_types.update(bigserialx: value))
+        .eq(All_types.c_bigserialx, oldValue);
+    return null;
+  } catch (error) {
+    print("updateBigSerial error");
+    print(error);
+    return error;
+  }
+}
