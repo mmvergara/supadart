975195eb1b289e80dad6726c0a7c65842760da97
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-22 13:11:03 +0800

    Add version flag and enable/disable Dart mode

--- cli/bin/supadart.dart
+++ cli/bin/supadart.dart
@@ -5,6 +5,8 @@ import 'package:dotenv/dotenv.dart';
 import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) async {
+  const String version = 'v1.2.0';
+
   final parser = ArgParser()
     ..addFlag('help',
         abbr: 'h', negatable: false, help: 'Show usage information')
@@ -18,7 +20,12 @@ void main(List<String> arguments) async {
         help: 'Supabase ANON KEY     -- (default: .env SUPABASE_ANON_KEY)')
     ..addOption('output',
         abbr: 'o',
-        help: 'Output file path      -- (default: lib/generated_classes.dart)');
+        help: 'Output file path      -- (default: lib/generated_classes.dart)')
+    ..addFlag('dart',
+        abbr: 'd',
+        negatable: false,
+        help: 'Enable if you are not using Flutter, just normal Dart project')
+    ..addFlag('version', abbr: 'v', negatable: false, help: version);
 
   final results = parser.parse(arguments);
 
@@ -27,6 +34,13 @@ void main(List<String> arguments) async {
     print(parser.usage);
     exit(0);
   }
+
+  if (results['version']) {
+    print(version);
+    exit(0);
+  }
+
+  bool isDart = results['dart'] ?? false;
 
   String? url;
   String? anonKey;
@@ -52,7 +66,7 @@ void main(List<String> arguments) async {
     exit(1);
   }
 
-  String? codeOutput = await getGeneratedClasses(url, anonKey);
+  String? codeOutput = await getGeneratedClasses(url, anonKey, isDart);
   if (codeOutput == null) {
     print('Failed to generate classes');
     exit(1);
@@ -67,9 +81,14 @@ void main(List<String> arguments) async {
 }
 
 Future<String?> getGeneratedClasses(
-    String supabaseUrl, String supabaseAnonKey) async {
+    String supabaseUrl, String supabaseAnonKey, bool isDart) async {
   String url =
       "https://supabase-schema-dart-class-generator.vercel.app/api/generate?SUPABASE_URL=$supabaseUrl&SUPABASE_ANON_KEY=$supabaseAnonKey";
+
+  if (isDart) {
+    url += "&dart=true";
+  }
+
   try {
     var response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
