234d3ee824d61785c174aa64c49143ec057b8d7d
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-22 21:32:24 +0800

    Remove unused import and function

--- test/test/utils.dart
+++ test/test/utils.dart
@@ -1,6 +1,5 @@
 import 'package:supabase/supabase.dart';
 import 'package:supadart_test/generated_classes.dart';
-import 'dart:io';
 
 Future<void> cleanup(SupabaseClient supabase) async {
   try {
@@ -9,28 +8,4 @@ Future<void> cleanup(SupabaseClient supabase) async {
     print("Error cleaning up");
     print(err);
   }
-}
-
-Future<void> changeSupabaseFlutterImportToSupabaseImport() async {
-  // Define the path to the file
-  final filePath = './lib/hello.dart';
-
-  final file = File(filePath);
-  if (!await file.exists()) {
-    print('File does not exist.');
-    return;
-  }
-
-  List<String> lines = await file.readAsLines();
-
-  if (lines.isNotEmpty) {
-    lines[0] = "import 'package:supabase/supabase.dart';";
-  } else {
-    lines.add("import 'package:supabase/supabase.dart';");
-  }
-
-  // Write the modified content back to the file
-  await file.writeAsString(lines.join('\n'));
-
-  print('File modified successfully.');
 }
