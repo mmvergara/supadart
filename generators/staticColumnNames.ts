0093427b5fcde9bcb36703043c5b0b391f92f614
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-22 22:38:15 +0800

    Remove console.log statements from generateStaticColumnNames function

--- generators/staticColumnNames.ts
+++ generators/staticColumnNames.ts
@@ -3,9 +3,7 @@ import { Properties } from "./types";
 export const generateStaticColumnNames = (properties: Properties) => {
   let code = "";
   for (const propertyName in properties) {
-    console.log(propertyName);
     code += `static String get c_${propertyName} => '${propertyName}';\n`;
   }
-  console.log(code);
   return code;
 };
