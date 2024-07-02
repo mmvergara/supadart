800e2501a3fb4babdaba71c8636ad98532722da3
mmvergara <mark.jesusmanabat@gmail.com>
2024-07-02 19:33:55 +0800

    feat: Add abstract generated class for code generation

---
+++ generators/AbstractGeneratedClass.ts
@@ -0,0 +1,20 @@
+export const AbstractGeneratedClass = `
+abstract class GeneratedClass<T> {
+  static String get table_name {
+    throw UnimplementedError();
+  }
+
+  static String get c_id => 'id';
+
+  static Map<String, dynamic> insert(Map<String, dynamic> data) {
+    throw UnimplementedError();
+  }
+
+  static Map<String, dynamic> update(Map<String, dynamic> data) {
+    throw UnimplementedError();
+  }
+
+  factory GeneratedClass.fromJson(Map<String, dynamic> json) {
+    throw UnimplementedError();
+  }
+}`;
