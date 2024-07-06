4cd62bead0c7acda5601cbf22deeb4670c51c859
mmvergara <mark.jesusmanabat@gmail.com>
2024-07-06 13:47:11 +0800

    Refactor AbstractGeneratedClass to remove unused code

--- generators/AbstractGeneratedClass.ts
+++ generators/AbstractGeneratedClass.ts
@@ -1,11 +1,5 @@
 export const AbstractGeneratedClass = `
 abstract class GeneratedClass<T> {
-  static String get table_name {
-    throw UnimplementedError();
-  }
-
-  static String get c_id => 'id';
-
   static Map<String, dynamic> insert(Map<String, dynamic> data) {
     throw UnimplementedError();
   }
