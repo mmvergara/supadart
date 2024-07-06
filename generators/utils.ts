af078c814bb3ddbdd508920c1b0ea3b66b635caf
mmvergara <mark.jesusmanabat@gmail.com>
2024-07-06 18:01:24 +0800

    fix json encoding

--- generators/utils.ts
+++ generators/utils.ts
@@ -194,6 +194,8 @@ export const toJsonEncodable = (
       }
       parseValue = `${propertyName}.toIso8601String()`;
       break;
+    case "Map<String, dynamic>":
+      return propertyName;
     case "List<Map<String, dynamic>>":
       console.log("List<Map<String, dynamic>>", propertyName);
       parseValue = `jsonEncode(${propertyName})`;
