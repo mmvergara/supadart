7661b5820370c1fcdb6d4021bc08b74e5d16e489
mmvergara <mark.jesusmanabat@gmail.com>
2024-07-17 10:09:58 +0800

    Refactor converter methods in generated Dart classes

--- generators/converterMethods.ts
+++ generators/converterMethods.ts
@@ -1,14 +1,14 @@
 export const generateConverterMethod = (className: string) => {
-  let code = `static List<${className}>? converter(List<Map<String, dynamic>> data) {\n`;
-  code += `return data.map((data) => ${className}.fromJson(data)).toList();\n`;
+  let code = `static List<${className}> converter(List<Map<String, dynamic>> data) {\n`;
+  code += `return data.map(${className}.fromJson).toList();\n`;
   code += `}\n`;
   return code;
 };
 
 export const generateConverterSingleMethod = (className: string) => {
-  let code = `static ${className}? converterSingle(Map<String, dynamic>? data) {\n`;
+  let code = `static List<${className}>? converterSingle(Map<String, dynamic>? data) {\n`;
   code += `if (data == null) return null;\n`;
-  code += `return ${className}.fromJson(data);\n`;
+  code += `return [${className}.fromJson(data)];\n`;
   code += `}\n`;
   return code;
 };
