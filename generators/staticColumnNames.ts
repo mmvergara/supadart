65a829287835937450c8f3b6e0a03b73d5ad0f76
mmvergara <mark.jesusmanabat@gmail.com>
2024-08-11 12:48:45 +0800

    remove old nextjs website

--- generators/staticColumnNames.ts
+++
@@ -1,9 +0,0 @@
-import { Properties } from "./types";
-
-export const generateStaticColumnNames = (properties: Properties) => {
-  let code = "";
-  for (const propertyName in properties) {
-    code += `static String get c_${propertyName} => '${propertyName}';\n`;
-  }
-  return code + "\n";
-};
