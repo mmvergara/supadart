a084c9bf6c2cd8802caf8c4d9dfe2b947647d492
mmvergara <mark.jesusmanabat@gmail.com>
2024-09-05 12:26:43 +0800

    Refactor toJsonEncodable function to handle boolean values

--- cli/lib/generators/utils/to_json_encodable.dart
+++ cli/lib/generators/utils/to_json_encodable.dart
@@ -1,66 +1,166 @@
 import '../swagger/column.dart';
 
 String toJsonEncodable(
-  String dartType,
-  String format,
-  String columnName,
   Column columnDetails,
 ) {
+  final columnName = columnDetails.camelColName;
+  String format = columnDetails.postgresFormat;
   bool isArray = format.contains("[]");
-  if (isArray) {
-    format = format.replaceAll("[]", "");
-  }
 
-  String parseValue = "";
-  switch (dartType) {
-    case "bool":
-      parseValue = columnName;
+  String jsonEncodableType = '';
+  switch (format) {
+    case 'smallint':
+    case 'integer':
+    case 'smallint[]':
+    case 'integer[]':
+      jsonEncodableType = columnName;
       break;
-    case "DateTime":
-      if (format == "time without time zone") {
-        parseValue = isArray
-            ? "$columnName.map((e) => DateFormat('HH:mm:ss.SSS').format(e)).toList()"
-            : "DateFormat('HH:mm:ss.SSS').format($columnName)";
-        break;
-      }
-      if (format == "time with time zone") {
-        parseValue = isArray
-            ? "$columnName.map((e) => DateFormat('HH:mm:ss zzzz').format(e)).toList()"
-            : "DateFormat('HH:mm:ss zzzz').format($columnName)";
-        break;
-      }
-      if (format == "timestamp with time zone") {
-        parseValue = isArray
-            ? '$columnName.map((e) => e.toUtc().toString()).toList()'
-            : '$columnName.toUtc().toString()';
-        break;
-      }
-      parseValue = '$columnName.toIso8601String()';
+
+    case 'bigint':
+      jsonEncodableType = "$columnName.toString()";
       break;
-    case "Map<String, dynamic>":
-      return columnName;
-    case "List<Map<String, dynamic>>":
-      parseValue = 'jsonEncode($columnName)';
+    case 'bigint[]':
+      jsonEncodableType = "$columnName.map((e) => e.toString()).toList()";
       break;
+
+    case 'real':
+    case 'real[]':
+      jsonEncodableType = columnName;
+
+    case 'double precision':
+    case 'numeric':
+      jsonEncodableType = '$columnName.toString()';
+      break;
+    case 'double precision[]':
+    case 'numeric[]':
+      jsonEncodableType = "$columnName.map((e) => e.toString()).toList()";
+      break;
+
+    case 'json':
+    case 'jsonb':
+      jsonEncodableType = columnName;
+      break;
+
+    case 'json[]':
+    case 'jsonb[]':
+      jsonEncodableType = 'jsonEncode($columnName)';
+      jsonEncodableType += '''
+      .replaceAll('"', '\\\\\"')
+      .replaceAll("{", '"{')
+      .replaceAll("}", '}"')
+      .replaceAll("[", '{')
+      .replaceAll("]", '}')''';
+      break;
+
+    case 'text':
+    case 'character varying':
+    case 'character':
+    case 'uuid':
+      jsonEncodableType = columnName;
+      break;
+    case 'text[]':
+    case 'character varying[]':
+    case 'character[]':
+    case 'uuid[]':
+      jsonEncodableType = "$columnName.map((e) => e).toList()";
+      break;
+
+    case 'date':
+    case 'timestamp without time zone':
+      jsonEncodableType = '$columnName.toIso8601String()';
+      break;
+    case 'date[]':
+    case 'timestamp without time zone[]':
+      jsonEncodableType =
+          '$columnName.map((e) => e.toIso8601String()).toList()';
+      break;
+    case 'time without time zone':
+      jsonEncodableType = "DateFormat('HH:mm:ss.SSS').format($columnName)";
+      break;
+    case 'time without time zone[]':
+      jsonEncodableType =
+          "$columnName.map((e) => DateFormat('HH:mm:ss.SSS').format(e)).toList()";
+      break;
+    case 'time with time zone':
+      jsonEncodableType = "DateFormat('HH:mm:ss zzzz').format($columnName)";
+      break;
+    case 'time with time zone[]':
+      jsonEncodableType =
+          "$columnName.map((e) => DateFormat('HH:mm:ss zzzz').format(e)).toList()";
+      break;
+    case 'timestamp with time zone':
+      jsonEncodableType = '$columnName.toUtc().toIso8601String()';
+      break;
+    case 'timestamp with time zone[]':
+      jsonEncodableType =
+          '$columnName.map((e) => e.toUtc().toIso8601String()).toList()';
+      break;
+    case 'boolean':
+      jsonEncodableType = columnName;
+      break;
+    case 'boolean[]':
+      jsonEncodableType = "$columnName.map((e) => e).toList()";
+      break;
+
+    // NOT YET SUPPORTED TYPES ARE ENCODED TO STRINGS BY DEFAULT
+    // NEED CONTRIBUTIONS TO SUPPORT THESE TYPES
+    case 'interval':
+    case 'bytea':
+    case 'inet':
+    case 'cidr':
+    case 'macaddr':
+    case 'macaddr8':
+    case 'point':
+    case 'line':
+    case 'lseg':
+    case 'box':
+    case 'path':
+    case 'polygon':
+    case 'circle':
+    case 'bit':
+    case 'bit varying':
+    case 'money':
+    case 'tsquery':
+    case 'tsvector':
+    case 'xml':
+    case 'pg_lsn':
+    case 'pg_snapshot':
+    case 'txid_snapshot':
+      jsonEncodableType = '$columnName.toString()';
+      break;
+    case 'interval[]':
+    case 'bytea[]':
+    case 'inet[]':
+    case 'cidr[]':
+    case 'macaddr[]':
+    case 'macaddr8[]':
+    case 'point[]':
+    case 'line[]':
+    case 'lseg[]':
+    case 'box[]':
+    case 'path[]':
+    case 'polygon[]':
+    case 'circle[]':
+    case 'bit[]':
+    case 'bit varying[]':
+    case 'money[]':
+    case 'tsquery[]':
+    case 'tsvector[]':
+    case 'xml[]':
+    case 'pg_lsn[]':
+    case 'pg_snapshot[]':
+    case 'txid_snapshot[]':
+      jsonEncodableType = "$columnName.map((e) => e.toString()).toList()";
+      break;
+
     default:
+      // If no format is provided, we assume it's a Enum
       if (columnDetails.enumValues.isNotEmpty) {
-        parseValue = isArray
+        jsonEncodableType = isArray
             ? "$columnName.map((e) => e.toString().split('.').last).toList()"
             : "$columnName.toString().split('.').last";
         break;
       }
-      parseValue = '$columnName.toString()';
   }
-
-  if (isArray && dartType == "List<Map<String, dynamic>>") {
-    parseValue += '''
-    .replaceAll('"', '\\\\\"')
-    .replaceAll("{", '"{')
-    .replaceAll("}", '}"')
-    .replaceAll("[", '{')
-    .replaceAll("]", '}')''';
-  } else if (isArray) {
-    parseValue += '.replaceAll("[", "{").replaceAll("]", "}")';
-  }
-  return parseValue;
+  return jsonEncodableType;
 }
