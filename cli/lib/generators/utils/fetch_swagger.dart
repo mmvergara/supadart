f6d810a76e2c61b5f24f4b65263ba2f658e130a2
mmvergara <mark.jesusmanabat@gmail.com>
2024-09-12 07:37:02 +0800

    remove print log when fetching

--- cli/lib/generators/utils/fetch_swagger.dart
+++ cli/lib/generators/utils/fetch_swagger.dart
@@ -13,7 +13,6 @@ Future<DatabaseSwagger?> fetchDatabaseSwagger(
     // First attempt with API key
     final response = await _secureRequest('$url/rest/v1/?apikey=$anonKey');
     if (response.statusCode == 200) {
-      print("Fetched Supabase Swagger with API key!");
       return DatabaseSwagger.fromJson(jsonDecode(response.body));
     }
 
@@ -21,7 +20,6 @@ Future<DatabaseSwagger?> fetchDatabaseSwagger(
     print("Trying without the API key...");
     final response2 = await _secureRequest('$url/rest/v1/');
     if (response2.statusCode == 200) {
-      print("Fetched Supabase Swagger without API key!");
       return DatabaseSwagger.fromJson(jsonDecode(response2.body));
     }
 
