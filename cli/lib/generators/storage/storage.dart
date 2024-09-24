37f14f32a517a01e8e46d140c77ac0c1cfdb10f3
mmvergara <mark.jesusmanabat@gmail.com>
2024-09-25 05:30:06 +0800

    imported a class from supabase/flutter package

--- cli/lib/generators/storage/storage.dart
+++ cli/lib/generators/storage/storage.dart
@@ -1,4 +1,39 @@
-import 'package:supabase/supabase.dart';
+// This class is copied from https://github.com/supabase/supabase-flutter
+// and is used to generate the storage client extension
+// We need to keep it here to avoid having to add the supabase package as a dependency
+class Bucket {
+  final String id;
+  final String name;
+  final String owner;
+  final String createdAt;
+  final String updatedAt;
+  final bool public;
+  final int? fileSizeLimit;
+  final List<String>? allowedMimeTypes;
+
+  const Bucket({
+    required this.id,
+    required this.name,
+    required this.owner,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.public,
+    this.fileSizeLimit,
+    this.allowedMimeTypes,
+  });
+
+  Bucket.fromJson(Map<String, dynamic> json)
+      : id = (json)['id'] as String,
+        name = json['name'] as String,
+        owner = json['owner'] as String,
+        createdAt = json['created_at'] as String,
+        updatedAt = json['updated_at'] as String,
+        public = json['public'] as bool,
+        fileSizeLimit = json['file_size_limit'] as int?,
+        allowedMimeTypes = json['allowed_mime_types'] == null
+            ? null
+            : List<String>.from(json['allowed_mime_types'] as List);
+}
 
 class Storage {
   final List<Bucket> buckets;
