ac713bee4c4a52b8b07593243e12f5d5dc13db95
mmvergara <mark.jesusmanabat@gmail.com>
2024-10-05 08:01:44 +0800

    bump to 1.6.4

--- cli/bin/supadart.dart
+++ cli/bin/supadart.dart
@@ -6,7 +6,7 @@ import 'package:supadart/generators/storage/fetch_storage.dart';
 import 'package:supadart/generators/utils/fetch_swagger.dart';
 import 'package:yaml/yaml.dart';
 
-const String version = 'v1.6.3';
+const String version = 'v1.6.4';
 const String red = '\x1B[31m';
 const String green = '\x1B[32m';
 const String blue = '\x1B[34m';
