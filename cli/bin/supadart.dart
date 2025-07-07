02bbd5dee487e44b6258cbac75ab916a307104ed
mmvergara <mark.jesusmanabat@gmail.com>
2025-07-08 06:21:14 +0800

    Release version 1.8.3: Fix default null type handling in fromJson Geometry, update version in pubspec.yaml and supadart.dart

--- cli/bin/supadart.dart
+++ cli/bin/supadart.dart
@@ -8,7 +8,7 @@ import 'package:supadart/generators/storage/fetch_storage.dart';
 import 'package:supadart/generators/utils/fetch_swagger.dart';
 import 'package:yaml/yaml.dart';
 
-const String version = 'v1.8.2';
+const String version = 'v1.8.3';
 const String red = '\x1B[31m';
 const String green = '\x1B[32m';
 const String blue = '\x1B[34m';
