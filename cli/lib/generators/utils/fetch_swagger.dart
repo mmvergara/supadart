3af4f85a2d8185846f161c95c879d2d0eb2298aa
mmvergara <mark.jesusmanabat@gmail.com>
2025-08-09 15:17:08 +0800

    Enhance fetchDatabaseSwagger to accept an optional schema parameter for custom headers, improving API request flexibility.

--- cli/lib/generators/utils/fetch_swagger.dart
+++ cli/lib/generators/utils/fetch_swagger.dart
@@ -4,14 +4,22 @@ import 'package:http/http.dart' as http;
 import '../swagger/swagger.dart';
 
 Future<DatabaseSwagger?> fetchDatabaseSwagger(String url, String anonKey,
-    Map<String, List<String>> mapOfEnums, bool jsonbToDynamic) async {
+    Map<String, List<String>> mapOfEnums, bool jsonbToDynamic,
+    {String? schema}) async {
   if (!url.startsWith("http://") && !url.startsWith("https://")) {
     url = "https://$url"; // Default to HTTPS if no scheme is provided
   }
 
+  // Prepare headers with accept-profile if schema is provided
+  Map<String, String>? headers;
+  if (schema != null && schema.isNotEmpty) {
+    headers = {'accept-profile': schema};
+  }
+
   try {
     // First attempt with API key in header
-    final response = await _secureRequest('$url/rest/v1/?apikey=$anonKey');
+    final response =
+        await _secureRequest('$url/rest/v1/?apikey=$anonKey', headers: headers);
     if (response.statusCode == 200) {
       return DatabaseSwagger.fromJson(
           jsonDecode(response.body), mapOfEnums, jsonbToDynamic);
@@ -19,7 +27,7 @@ Future<DatabaseSwagger?> fetchDatabaseSwagger(String url, String anonKey,
 
     // Second attempt without API key
     print("Trying without the API key...");
-    final response2 = await _secureRequest('$url/rest/v1/');
+    final response2 = await _secureRequest('$url/rest/v1/', headers: headers);
     if (response2.statusCode == 200) {
       return DatabaseSwagger.fromJson(
           jsonDecode(response2.body), mapOfEnums, jsonbToDynamic);
@@ -27,9 +35,10 @@ Future<DatabaseSwagger?> fetchDatabaseSwagger(String url, String anonKey,
 
     // Third attempt with API key in header
     print("Trying with the API key in header...");
-    final response3 = await _secureRequest('$url/rest/v1/', headers: {
-      'apikey': anonKey,
-    });
+    final headersWithApiKey = headers ?? <String, String>{};
+    headersWithApiKey['apikey'] = anonKey;
+    final response3 =
+        await _secureRequest('$url/rest/v1/', headers: headersWithApiKey);
     if (response3.statusCode == 200) {
       return DatabaseSwagger.fromJson(
           jsonDecode(response3.body), mapOfEnums, jsonbToDynamic);
